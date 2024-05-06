523b10d2fc806ef1db689f5bafeaf90699c92227
anton.a <anton.adamson@gmail.com>
2024-05-06 15:27:17 +0600

    Initial Commit

---
+++ TestAuthApp01/TestAuthApp01App.swift
@@ -0,0 +1,17 @@
+//
+//  TestAuthApp01App.swift
+//  TestAuthApp01
+//
+//  Created by Антон Адамсон on 06.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAuthApp01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
